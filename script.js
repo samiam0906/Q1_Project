ebb0e24022a633982f17a3e3d493b494ede2c58c
Sam Hoffman <samdhoffman@gmail.com>
2017-09-18 14:54:10 -0400

    Made youtube links embedable

--- script.js
+++ script.js
@@ -23,8 +23,14 @@ $.get("https://www.reddit.com/r/soccer.json", function(data) {
     // checks if submission to reddit is a video
     if (data.data.children[i].data.secure_media) {
       if (data.data.children[i].data.secure_media.oembed.type == "video") {
-        // add the gif urls
-        contentLinks.push(data.data.children[i].data.url);
+        // add the youtube embed urls
+        if(data.data.children[i].data.domain === "youtube.com") {
+          var youtubeLink = data.data.children[i].data.url.replace("watch?v=", "embed/");
+          contentLinks.push(youtubeLink);
+        } else {
+          // add the gif urls
+          contentLinks.push(data.data.children[i].data.url);
+        }
         // add the submission titles
         contentTitles.push(data.data.children[i].data.title);
         // add the reddit id of the submissions
@@ -64,17 +70,26 @@ $.get("https://www.reddit.com/r/soccer.json", function(data) {
     })
 
     // add id to the collapsible-body
-    $('main ul li').each(function(index) {
+    $('main ul li .collapsible-body').each(function(index) {
       $(this).attr('id', contentID[index]);
     })
 
     // adds the gif urls to each collapsible body
-    $('.collapsible-header').click(function(e) {
-      $('.collapsible-body').each(function(index) {
-        $(this).html('<iframe class="gif" src=' + contentLinks[index] + '>');
-      })
+    $('.collapsible-header').click('iframe', function(e) {
+      if ($(e.target).parent().hasClass('active')) {
+        $(this).next().next().html("");
+      }
+      // if (e.target.next().contains('iframe')) {
+      //   $('.collapsible-body').html("");
+      // }
+      // if ($(e.target).parent().hasClass('active')) {
+      //   $(e.target).next().innerHTML = "";
+      // }
+      $('.collapsible-body').html("");
+      var elementIndex = contentID.indexOf($(this).next().attr('id'));
+      // $(this).next().html('<div style="width: 100%; height: 0px; position: relative; padding-bottom: 56.250%;"><iframe src=' + contentLinks[elementIndex] + '/ylhkwh '+ 'frameborder="0" width="100%" height="100%" allowfullscreen style="width: 100%; height: 100%; position: absolute;"></iframe></div>')
+      $(this).next().html('<iframe class="gif" src=' + contentLinks[elementIndex] + '>');
     })
 
-    // $(this).html('<iframe src=' + contentLinks[index] + '>');
   })
 });
