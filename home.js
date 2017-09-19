a7c73ac660caae068cea5143b4b14c6f114b93f8
Sam Hoffman <samdhoffman@gmail.com>
2017-09-18 22:31:49 -0400

    Add Slideshow to Home Page

---
+++ home.js
@@ -0,0 +1,18 @@
+// $(document).ready(function() {
+//
+//   var count = 1;
+//   var images = ["images/naphtali-marshall-13585.jpg", "images/george-doumani-46147.jpg", "images/glen-carrie-102887.jpg", "images/omair-khan-163090.jpg"];
+//   var photo = $('.fader');
+//
+//   // photo.css("background-image", "url("+images[count]+")", "no-repeat", "center", "center", "fixed");
+//
+//   setInterval(function() {
+//     photo.fadeOut(500, function() {
+//       photo.css("background-image", "url("+images[count++]+")", "no-repeat", "center", "center", "fixed");
+//       photo.fadeIn(500);
+//     });
+//     if (count === images.length) {
+//       count = 0;
+//     }
+//   }, 2000);
+// });
